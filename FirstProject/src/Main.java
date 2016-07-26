4513dabab88e035a16adabca1e92b7136603fe58
StylishGirrafe <falconjawas@gmail.com>
2016-07-27 04:10:42 +0700

    Multiple classes + methods

--- FirstProject/src/Main.java
+++ FirstProject/src/Main.java
@@ -1,8 +1,37 @@
-
+import java.util.Scanner;
 public class Main {
+/*public class apples {
+	
+	public static void main(String[] args) {
+		//whatever we type in keyboard will be held by yoyo
+	Scanner yoyo = new Scanner(System.in);
+	double fnum, snum, answer;
+	System.out.println("Enter first num: ");
+	fnum = yoyo.nextDouble();
+	System.out.println("Enter second num: ");
+	snum = yoyo.nextDouble();
+	answer = fnum+snum;
+	System.out.println(answer);
+	}
+
+}*/
 	public static void main(String[] args){
 		//create object
+		/*tuna tunaObject = new tuna();
+		tunaObject.simpleMessage();*/
+		
+		//make scanner object
+		Scanner input = new Scanner(System.in);
 		tuna tunaObject = new tuna();
-		tunaObject.simpleMessage();
+		
+		System.out.println("Enter your name here: ");
+		String name = input.nextLine();
+		
+		//tunaObject.simpleMessage(name);
+		
+		System.out.println("Enter name of first gf:");
+		String temp = input.nextLine();
+		tunaObject.setName(temp);
+		tunaObject.saying();
 	}
 }
--- FirstProject/src/tuna.java
+++ FirstProject/src/tuna.java
@@ -1,8 +1,25 @@
 
 public class tuna {
 
-	public static void simpleMessage(){
+	/*public static void simpleMessage(){
 		System.out.println("Heyho");
+	}*/
+	
+	/*public static void simpleMessage(String name){
+		System.out.println("Hello " + name);
+	}*/
+	
+	private String girlName;
+	
+	public void setName(String name){
+		girlName = name;
 	}
-
+	
+	public String getName(){
+		return girlName;
+	}
+	
+	public void saying(){
+		System.out.printf("Your first gf was %s", getName());
+	}
 }
