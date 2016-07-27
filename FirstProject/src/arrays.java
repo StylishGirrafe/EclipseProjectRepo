7c3638d4446e2201eb2d1391bb81f0409d03340f
StylishGirrafe <falconjawas@gmail.com>
2016-07-28 05:31:07 +0700

    Adding array

---
+++ FirstProject/src/arrays.java
@@ -0,0 +1,15 @@
+
+public class arrays {
+	public void yaho(){
+	int testArr[] = new int[10]; //how many values you gonna store
+	
+	//cara isi array nya
+	int testArray[] = {2,4,5,9};
+	//or
+	testArr[0] = 87;
+	testArr[1] = 543;
+	
+	
+	System.out.println(testArr[1]);
+	}
+}
