d1292a5a7c7910b1cf32e20b0256c65d5587c68b
StylishGirrafe <falconjawas@gmail.com>
2016-07-31 16:10:13 +0700

    Add sum function to arrayTable

--- FirstProject/src/arrayTable.java
+++ FirstProject/src/arrayTable.java
@@ -3,9 +3,12 @@ public class arrayTable {
 	public void arrayTab(){
 		System.out.println("Index\tValue");
 		int bucky[] = {32,12,18,54,2};
+		int sum = 0;
 		
 		for(int counter = 0; counter<bucky.length; counter++){
-			System.out.println(counter + "\t" + bucky[counter]);
+			//System.out.println(counter + "\t" + bucky[counter]);
+			sum += bucky[counter];
 		}
+		System.out.println("The sum of this number is: "+ sum);
 	}
 }
