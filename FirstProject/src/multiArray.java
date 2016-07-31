5b490269e9d4b091b1c1860c41f6c99ec26d4f94
StylishGirrafe <falconjawas@gmail.com>
2016-08-01 03:24:46 +0700

    Add main method to multi dimensional array file

--- FirstProject/src/multiArray.java
+++ FirstProject/src/multiArray.java
@@ -1,7 +1,11 @@
 
 public class multiArray {
 	
-	public void ma(){
+	public static void main(String[]args){
+		ma();
+	}
+	
+	public static void ma(){
 		int firstArray[][]={{8,9,10,11},{12,13,14,15}};
 		int secondArray[][] = {{30,31,32,33},{43},{4,5,6}};
 	
