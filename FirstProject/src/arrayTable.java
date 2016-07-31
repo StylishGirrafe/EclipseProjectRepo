ab5133446f40000cea27e9dbfd172bad91d65c4d
StylishGirrafe <falconjawas@gmail.com>
2016-08-01 03:04:47 +0700

    Adding multidimensional array

--- FirstProject/src/arrayTable.java
+++ FirstProject/src/arrayTable.java
@@ -11,4 +11,27 @@ public class arrayTable {
 		}
 		System.out.println("The sum of this number is: "+ sum);
 	}
+	//or use this alternatives (to loop through all the array values)
+	public void enhancedLoop(){
+		int buckys[] = {3,4,5,6,7};
+		int total = 0;
+		
+		for(int x: buckys){
+			total += x;
+		}
+		
+		System.out.println(total);
+	}
+	
+	//arrays in methods
+	public void arrayMethod(){
+		int buckys[] = {3,4,5,6,7};
+		change (buckys);
+	}
+	
+	public static void change(int x[]){
+		for(int counter=0; counter<x.length; counter++){
+			x[counter] += 5;
+		}
+	}
 }
